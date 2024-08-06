07809e7b30f125c07f2a5506cefca9f2083631a6
KomailShah05 <syedkomail121@gmail.com>
2024-08-06 17:49:36 +0500

    Added PivotTableUI from react-pivottable

--- src/pages/fmsca-table/index.tsx
+++ src/pages/fmsca-table/index.tsx
@@ -1,17 +1,13 @@
 // libraries
 import * as React from "react";
-import { useState } from "react";
+import { useState, useMemo, useEffect } from "react";
 import axios from "axios";
 import {
   Box,
   CircularProgress,
   Paper,
-  Table,
-  TableBody,
-  TableCell,
   TableContainer,
   TablePagination,
-  TableRow,
   Typography,
 } from "@mui/material";
 
@@ -19,6 +15,9 @@ import {
 import TableHeader from "../../components/fmsca-table/AppHeader";
 import Filter from "../../components/fmsca-table/AppFilter";
 import TopAppBar from "../../components/fmsca-table/AppBar";
+import PivotTableUI from "react-pivottable/PivotTableUI";
+import "react-pivottable/pivottable.css";
+import TableRenderers from 'react-pivottable/TableRenderers';
 
 // constants
 import { COLUMNS_TO_INCLUDE } from "../../config/constants";
@@ -27,21 +26,32 @@ import AppFooter from "../../components/fmsca-table/AppFooter";
 interface RowData {
   [key: string]: string;
 }
+
+const defaultPivotState = {
+  rows: ['created_dt', 'data_source_modified_dt', 'entity_type', 'operating_status', 'legal_name', 'dba_name', 'physical_address', 'phone', 'usdot_number', 'mc_mx_ff_number', 'power_units', 'out_of_service_date'], // Replace 'defaultRowField' with your actual field name
+  // cols: ['legal_name'], // Replace 'defaultColField' with your actual field name
+  aggregatorName: 'Count', // Replace with your desired aggregator
+  vals: [],
+};
 const RANGE = "FMSCA_records (2)"; // Adjust the range based on your data
 
 export default function FMCATable() {
   const [data, setData] = useState<RowData[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [filters, setFilters] = useState<{ [key: string]: string }>({}); // Store filter values for each column
+  const [filters, setFilters] = useState<{ [key: string]: string }>({});
   const [isDataLoading, setIsDataLoading] = useState(false);
+  const [pivotState, setPivotState] = useState(defaultPivotState);
+  const [paginatedData, setPaginatedData] = useState<RowData[]>([]);
 
-  //  functions loads data from spreadsheet
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  // Fetch data from the spreadsheet
   const fetchData = async () => {
     try {
       setIsDataLoading(true);
       const response = await axios.get<{ values: string[][] }>(
-        `https://sheets.googleapis.com/v4/spreadsheets/1hB_LjBT9ezZigXnC-MblT2PXZledkZqBnvV23ssfSuE/values/${RANGE}?key=AIzaSyABMiPUGdBbbjQBovGe6Lx4AJxK-1yasyE`
+        `https://sheets.googleapis.com/v4/spreadsheets/1hB_LjBT9ezZigXnC-MblT2PXZledkZqBnvV23ssfSuE/values/${RANGE}?key=${apiKey}`
       );
 
       const rows = response.data.values;
@@ -69,15 +79,16 @@ export default function FMCATable() {
       }
       setIsDataLoading(false);
     } catch (error) {
+      console.error("Error fetching data:", error);
       setIsDataLoading(false);
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchData();
   }, []);
 
-  // function is called when the page number has changed
+  // Update page number
   const handlePageChange = (
     event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number
@@ -85,7 +96,7 @@ export default function FMCATable() {
     setPage(newPage);
   };
 
-  // function is called when the page lenght has changed
+  // Update rows per page
   const handleRowsPerPageChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -93,7 +104,7 @@ export default function FMCATable() {
     setPage(0);
   };
 
-  // function is called when the user type in textfeild to search
+  // Update filters
   const handleFilterChange =
     (column: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
       setFilters({
@@ -102,17 +113,27 @@ export default function FMCATable() {
       });
     };
 
-  const filteredData = data.filter((row) => {
-    return Object.keys(filters).every((key) => {
-      return row[key]
-        ?.toString()
-        .toLowerCase()
-        .includes(filters[key].toLowerCase());
-    });
-  });
   const resetFilters = () => {
     setFilters({});
   };
+
+  const filteredData = useMemo(() => {
+    return data.filter((row) => {
+      return Object.keys(filters).every((key) => {
+        return row[key]
+          ?.toString()
+          .toLowerCase()
+          .includes(filters[key].toLowerCase());
+      });
+    });
+  }, [data, filters]);
+
+  useEffect(() => {
+    const start = page * rowsPerPage;
+    const end = start + rowsPerPage;
+    setPaginatedData(filteredData.slice(start, end));
+    console.log('Paginated Data:', filteredData.slice(start, end));
+  }, [filteredData, page, rowsPerPage]);
 
   return (
     <>
@@ -133,51 +154,43 @@ export default function FMCATable() {
               },
             }}
           >
-            <Table stickyHeader aria-label="sticky table">
-              <TableHeader />
-              <TableBody sx={{ position: "relative" }}>
-                {isDataLoading ? (
-                  <CircularProgress
-                    color="inherit"
-                    sx={{ position: "absolute", top: "30vh", left: "50%" }}
-                  />
-                ) : filteredData.length === 0 ? (
-                  <Typography
-                    sx={{ position: "absolute", top: "30vh", left: "50%" }}
-                  >
-                    No Data Found
-                  </Typography>
-                ) : (
-                  filteredData
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, index) => (
-                      <TableRow hover role="checkbox" tabIndex={-1} key={index}>
-                        {Object.keys(data[0] || {}).map((column) => {
-                          const value = row[column];
-                          return (
-                            <TableCell
-                              key={column}
-                              style={{ padding: 12, fontSize: ".6875rem" }}
-                            >
-                              {value}
-                            </TableCell>
-                          );
-                        })}
-                      </TableRow>
-                    ))
-                )}
-              </TableBody>
-            </Table>
+            {isDataLoading ? (
+              <CircularProgress
+                color="inherit"
+                sx={{ position: "absolute", top: "30vh", left: "50%" }}
+              />
+            ) : filteredData.length === 0 ? (
+              <Typography
+                sx={{ position: "absolute", top: "30vh", left: "50%" }}
+              >
+                No Data Found
+              </Typography>
+            ) : (
+              <>
+                <PivotTableUI
+
+                  data={paginatedData}
+                  onChange={(s: any) => {
+                    const newState = { ...s };
+                    delete newState.data;
+                    setPivotState(newState);
+                  }}
+                  {...pivotState}
+                  renderers={{ ...TableRenderers, }}
+
+                />
+                <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={filteredData.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handlePageChange}
+                  onRowsPerPageChange={handleRowsPerPageChange}
+                />
+              </>
+            )}
           </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={filteredData.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handlePageChange}
-            onRowsPerPageChange={handleRowsPerPageChange}
-          />
         </Paper>
       </Box>
       <AppFooter />
